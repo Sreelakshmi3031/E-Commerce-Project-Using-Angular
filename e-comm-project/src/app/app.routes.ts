18f6dbf42dcdba784dc5a4c66dd1b35c61d76ae0
Sreelakshmi3031 <245524193+Sreelakshmi3031@users.noreply.github.com>
2025-11-22 21:52:17 +0530

    header files added

---
+++ e-comm-project/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { SellerAuth } from './seller-auth/seller-auth';
+import { SellerHome } from './seller-home/seller-home';
+import { authGuard } from './guards/auth-guard';
+import { SellerAddProduct } from './seller-add-product/seller-add-product';
+
+export const routes: Routes = [
+  {
+    component: Home,
+    path: '',
+  },
+  {
+    component: SellerAuth,
+    path: 'seller-auth',
+  },
+  {
+    component: SellerHome,
+    path: 'seller-home',
+    canActivate: [authGuard],
+  },
+  {
+    component: SellerAddProduct,
+    path: 'seller-add-product',
+    canActivate: [authGuard],
+  },
+];
---
+++ e-comm-project/src/app/header/header.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink, CommonModule],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header implements OnInit {
+  menuType: string = 'default';
+  sellerName: string = '';
+  constructor(private route: Router) {}
+
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          console.warn('seller area');
+          this.menuType = 'seller';
+          if (localStorage.getItem('seller')) {
+            let sellerStore = localStorage.getItem('seller');
+            let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+            this.sellerName = sellerData.name;
+          }
+        } else {
+          console.warn('outside seller');
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
+  }
+}
