de997cd467931983be64ff590d3ac0231e59994b
Sreelakshmi3031 <245524193+Sreelakshmi3031@users.noreply.github.com>
2025-11-24 21:59:54 +0530

    bootstrap carousel added in home page

--- e-comm-project/src/app/home/home.ts
+++ e-comm-project/src/app/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgbModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class Home {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
