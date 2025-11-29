03d3c0a94c2fc9190b3007f4a99eb4478057c021
Sreelakshmi3031 <245524193+Sreelakshmi3031@users.noreply.github.com>
2025-11-29 22:04:19 +0530

    footer added

--- e-comm-project/src/app/app.ts
+++ e-comm-project/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('e-comm-project');
