17ad11188994ac611b02ceeee971f84cd69c6a4f
Sreelakshmi3031 <245524193+Sreelakshmi3031@users.noreply.github.com>
2025-11-30 21:25:15 +0530

    local storage cart data to DB

--- e-comm-project/src/app/user-auth/user-auth.ts
+++ e-comm-project/src/app/user-auth/user-auth.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { login, signUp } from '../data-type';
+import { cart, login, product, signUp } from '../data-type';
 import { User } from '../services/user';
+import { Product } from '../services/product';
 
 @Component({
   selector: 'app-user-auth',
@@ -13,7 +14,7 @@ export class UserAuth implements OnInit {
   showLoginForm: boolean = false;
 
   invalidUserErrorMsg: string = '';
-  constructor(private user: User) {}
+  constructor(private user: User, private product: Product) {}
 
   ngOnInit(): void {
     this.user.userAuthReload();
@@ -28,6 +29,29 @@ export class UserAuth implements OnInit {
     this.user.invalidUserAuth.subscribe((result) => {
       if (result) {
         this.invalidUserErrorMsg = 'User does not exist';
+      } else {
+        let userData = localStorage.getItem('user');
+        let userId = userData && JSON.parse(userData).id;
+        let cartData = localStorage.getItem('localCartData');
+        if (cartData) {
+          let cartDataDetails: product[] = JSON.parse(cartData);
+          cartDataDetails.forEach((product: product, index) => {
+            let cartItem: cart = {
+              ...product,
+              productId: product.id,
+              userId,
+            };
+            delete cartItem.id;
+            setTimeout(() => {
+              this.product.addToCart(cartItem).subscribe((result) => {
+                console.warn(result);
+              });
+              if (cartDataDetails.length === index + 1) {
+                localStorage.removeItem('localCartData');
+              }
+            }, 500);
+          });
+        }
       }
     });
   }
